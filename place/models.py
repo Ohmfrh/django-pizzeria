1a5ae9516eeb04f466e881caeb7e219eb0795aa9
Daniel Castro <dancassig@gmail.com>
2018-01-31 19:07:20 -0600

    Created Pizza and Ingredient models

--- place/models.py
+++ place/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    cost = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    ingredients = models.ManyToManyField('place.Ingredient')
+
+    def __str__(self):
+        return f"{self.name} -> {self.ingredients.all()}"
+
