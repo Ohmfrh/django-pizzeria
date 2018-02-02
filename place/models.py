a8f94fa46259533f58e703daf63dab9edde15eb6
Daniel Castro <dancassig@gmail.com>
2018-02-01 19:18:28 -0600

    Reset migrations and created through model PizzaRecipe

--- place/models.py
+++ place/models.py
@@ -16,8 +16,13 @@ class Pizza(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
-    ingredients = models.ManyToManyField('place.Ingredient')
+    ingredients = models.ManyToManyField('place.Ingredient', through='place.PizzaRecipe')
 
     def __str__(self):
         return f"{self.name} -> {self.ingredients.all()}"
 
+
+class PizzaRecipe(models.Model):
+    pizza_id = models.ForeignKey('place.Pizza', on_delete=models.CASCADE)
+    igredient_id = models.ForeignKey('place.Ingredient', on_delete=models.CASCADE)
+    portion = models.IntegerField()
