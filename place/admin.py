06109f86b361017bb6abe6268f7385bcfc60d0e2
Daniel Castro <dancassig@gmail.com>
2018-02-01 19:20:15 -0600

    Registered PizzaRecipe to admin site

--- place/admin.py
+++ place/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Pizza, Ingredient
-
+from .models import Pizza, Ingredient, PizzaRecipe
 
 admin.site.register(Pizza)
 admin.site.register(Ingredient)
+admin.site.register(PizzaRecipe)
