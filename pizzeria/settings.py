93b58f7e15d20baf378c71eeffb55939d8342a6f
Daniel Castro <dancassig@gmail.com>
2018-02-07 17:30:30 -0600

    Renamed directory

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'pizza.urls'
+ROOT_URLCONF = 'pizzeria.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'pizza.wsgi.application'
+WSGI_APPLICATION = 'pizzeria.wsgi.application'
 
 
 # Database
