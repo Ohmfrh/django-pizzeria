ff75127a078f7a0ba9e3908f28cb721178329705
Daniel Castro <dancassig@gmail.com>
2018-02-09 15:09:06 -0600

    Adds django_crispy, configued static files and adds bootstrap template for crispy forms

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'place',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
