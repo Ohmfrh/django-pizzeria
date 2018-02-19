2ccb289a05b7a19c263c5bc4bde8647f5166bcae
Daniel Castro <dancassig@gmail.com>
2018-02-19 15:12:42 -0600

    Added migrate

--- compose/production/django/gunicorn.sh
+++ compose/production/django/gunicorn.sh
@@ -6,4 +6,6 @@ set -o nounset
 
 
 python /app/manage.py collectstatic --noinput
+python /app/manage.py migrate
+python /app/manage.py migrate
 /usr/local/bin/gunicorn pizzeria.wsgi:application -w 4 -b 0.0.0.0:5000 --chdir=/app
